b5ad8c1991631f1cf5512b33d703a206a1b68165
letsdevapps <letsdevapps@gmail.com>
2026-07-07 15:47:43 -0300

    adiciona CORS porque frontends externos precisam autorização para acessar a API

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // rotas
 const usuariosRoutes = require('./routes/usuarios.routes');
